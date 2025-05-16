b46cb7c4d352a1227a2b23984666784589190d71
Pa$$w0rd <a26263842z@gmail.com>
2025-05-16 17:32:32 +0300

    feet:add automation PR script

--- PR.sh
+++ PR.sh
@@ -4,7 +4,7 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <branch_name> <commit_message>"
     exit 1
 fi
-if git branch --list feature/"$1" 2> /dev/null | grep -q; then
+if git branch --list feature/"$1" 2> /dev/null 2>&1 | grep -q; then
     git checkout feature/"$1"
 else
     git checkout -b feature/"$1"
